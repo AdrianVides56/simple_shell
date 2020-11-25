857b85aaca8d989c5cf25a77cced5970139df47c
Adrian5666 <62396843+AdrianVides56@users.noreply.github.com>
2020-11-24 21:03:40 -0500

    Added 126

--- errors.c
+++ errors.c
@@ -6,17 +6,19 @@
  */
 void errors(int errornum)
 {
-	char *errormsg = "Error\n", *notfound = "Command not found\n";
-	char *newline = "\n";
+        char *errormsg = "Error\n", *notfound = "Command not found\n";
+        char __attribute__((unused)) *newline = "\n";
 
-	if (errornum == -1)
-	{
-		write(STDOUT_FILENO, errormsg, _strlen(errormsg));
-		exit(EXIT_SUCCESS);
-	}
-	else if (errornum == 0)
-		exit(EXIT_SUCCESS);
+        if (errornum == -1)
+        {
+                write(STDOUT_FILENO, errormsg, _strlen(errormsg));
+                exit(EXIT_SUCCESS);
+        }
+        else if (errornum == 0)
+                exit(EXIT_SUCCESS);
 
-	else if (errornum == 127)
-		write(STDOUT_FILENO, notfound, _strlen(notfound));
+        else if (errornum == 127)
+                write(STDOUT_FILENO, notfound, _strlen(notfound));
+        else if (errornum == 126)
+                write(STDOUT_FILENO, "Failed Malloc", _strlen("Failed malloc"));
 }
