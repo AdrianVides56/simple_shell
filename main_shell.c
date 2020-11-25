41991609ff4c23d9f902eefa9c9c991016478dca
Adrian5666 <62396843+AdrianVides56@users.noreply.github.com>
2020-11-24 21:03:10 -0500

    Handling malloc

--- main_shell.c
+++ main_shell.c
@@ -7,38 +7,39 @@
  * @envp: enviroment pointer
  * Return: 0 Success
  */
-int main(int argc, char **argv, char **envp)
+int main(int __attribute__((unused)) argc, char __attribute__((unused)) **argv,
+         char **envp)
 {
-	char *line;
-	char *token = NULL, *token2[1024], *path;
-	size_t getln, reset, i;
-	pid_t child_pid;
+        char *line = NULL;
+        char *token = NULL, *token2[512], *path = NULL;
+        size_t i = 0;
+        pid_t child_pid;
 
-	while (1)
-	{
-		i = isatty(STDOUT_FILENO);
-		line = _getline(i);
+        while (1)
+        {
+                i = isatty(STDOUT_FILENO);
+                line = _getline(i);
+                if (strcmp(line, "exit\n") == 0)
+                        errors(0);
+                token = strtok(line, DELIM);
 
-		if (strcmp(line, "exit\n") == 0)
-			errors(0);
-		token = strtok(line, DELIM);
-		for (i = 0; token != NULL; i++)
-		{
-			token2[i] = token;
-			token = strtok(NULL, DELIM);
-		}
-		child_pid = fork();
-		if (child_pid == -1)
-			errors(-1);
-		if (child_pid == 0)
-		{
-			path = _getenv(envp, "PATH");
-			_execve(path, token2[0], token2);
-		}
-		else
-			 wait(NULL);
-		for (reset = 0; reset <= i; reset++)
-			token2[reset] = NULL;
-	}
-	return (0);
+                for (i = 0; token != NULL; i++)
+                {
+                        token2[i] = token;
+                        token = strtok(NULL, DELIM);
+                }
+                child_pid = fork();
+                if (child_pid == -1)
+                        errors(-1);
+                if (child_pid == 0)
+                {
+                        path = _getenv(envp, "PATH");
+                        if (execve(line, token2, NULL) == -1)
+                                _execve(path, token2[0], token2);
+                }
+                else
+                        wait(NULL);
+        }
+        free(line);
+        return (0);
 }
