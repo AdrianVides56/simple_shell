29cee765968275af847c0d0aacc8a2f6c94d0f7b
AdrianVides56 <adrianvj18@gmail.com>
2020-11-30 17:32:10 +0000

    Changed exit status

--- main_functions.c
+++ main_functions.c
@@ -27,7 +27,7 @@ char *_getline(size_t val_issaty, char **myenvp)
 	if (_strcmp(line, "exit\n") == 0)
 	{
 		free(line);
-		exit(2);
+		exit(EXIT_FAILURE);
 	}
 	if (_strcmp(line, "env\n") == 0)
 	{
